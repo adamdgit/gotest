79570935e0716ee09b199bc15250491d7749dbca
adamdgit <adam_d@outlook.com>
2024-12-24 18:02:12 +1100

    testing .env file import

--- backend/routes/api.go
+++ backend/routes/api.go
@@ -2,15 +2,18 @@ package routes
 
 import (
 	"database/sql"
+	"fmt"
+	"os"
 
 	"github.com/adamdgit/gotest/backend/queries"
 	"github.com/gofiber/fiber/v2"
 )
 
 func RegisterAPIRoutes(app *fiber.App, db *sql.DB) {
+	apiVersion := os.Getenv("API_VERSION")
 
 	// Routes
-	app.Get("/api/v1/posts", func(c *fiber.Ctx) error {
+	app.Get(fmt.Sprintf("/api/%s/posts", apiVersion), func(c *fiber.Ctx) error {
 		posts, err := queries.GetAllPosts(app, db)
 		if err != nil {
 			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
@@ -21,7 +24,7 @@ func RegisterAPIRoutes(app *fiber.App, db *sql.DB) {
 		return c.JSON(posts)
 	})
 
-	app.Get("/api/v1/posts/:id", func(c *fiber.Ctx) error {
+	app.Get(fmt.Sprintf("/api/%s/posts/:id", apiVersion), func(c *fiber.Ctx) error {
 		id := c.Params("id")
 		return c.JSON("hello", id)
 	})
