f3ac96c038db530c88488d516caf1024adda22bc
adamdgit <adam_d@outlook.com>
2024-12-26 19:55:02 +1100

    added env support

--- backend/go.mod
+++ backend/go.mod
@@ -5,6 +5,7 @@ go 1.23.0
 require (
 	github.com/go-sql-driver/mysql v1.8.1
 	github.com/gofiber/fiber/v2 v2.52.5
+	github.com/joho/godotenv v1.5.1
 )
 
 require (
--- backend/main.go
+++ backend/main.go
@@ -10,6 +10,7 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/joho/godotenv"
 
 	"github.com/adamdgit/gotest/backend/routes"
 )
@@ -17,13 +18,19 @@ import (
 const PORT = ":8081"
 
 func main() {
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
 	username := os.Getenv("DB_USERNAME")
 	password := os.Getenv("DB_PASSWORD")
 	address := os.Getenv("DB_ADDRESS")
 	dbname := os.Getenv("DB_NAME")
 
 	// Initialise MySQL connection string
-	conn := fmt.Sprintf("%s:%s@tcp(%s)/%s", username, password, address, dbname)
+	conn := fmt.Sprintf("%s:%s@tcp(%s)/%s?parseTime=true", username, password, address, dbname)
 
 	db, err := sql.Open("mysql", conn)
 	if err != nil {
@@ -48,5 +55,5 @@ func main() {
 
 	// Listen on port
 	log.Fatal(app.Listen(PORT))
-	println("Listening on ", PORT)
+	log.Printf("Listening on ", PORT)
 }
