133224ad6720065e888d9e6f978c0a51c3230141
adamdgit <adam_d@outlook.com>
2024-12-27 19:30:22 +1100

    Added User struct

--- backend/models/models.go
+++ backend/models/models.go
@@ -11,3 +11,14 @@ type Post struct {
 	Created_At time.Time `json:"created_at"`
 	Updated_At time.Time `json:"updated_at"`
 }
+
+type User struct {
+	ID          int       `json:"id"`
+	Username    string    `json:"username"`
+	Password    string    `json:"password"`
+	Email       string    `json:"email"`
+	Role        string    `json:"role"`        // Admin, Staff, etc
+	Profile_URL string    `json:"profile_url"` // profile picture url to file
+	Created_At  time.Time `json:"created_at"`
+	Updated_At  time.Time `json:"updated_at"`
+}
