6c015f2310b755eab592cffbfa0a4995a00ebbfb
adamdgit <adam_d@outlook.com>
2024-12-28 18:35:26 +1100

    Added user roles model

--- backend/models/models.go
+++ backend/models/models.go
@@ -12,12 +12,20 @@ type Post struct {
 	Updated_At time.Time `json:"updated_at"`
 }
 
+type UserRole string
+
+const (
+	Admin  UserRole = "admin"
+	Staff  UserRole = "staff"
+	Member UserRole = "member"
+)
+
 type User struct {
 	ID          int       `json:"id"`
 	Username    string    `json:"username"`
 	Password    string    `json:"password"`
 	Email       string    `json:"email"`
-	Role        string    `json:"role"`        // Admin, Staff, etc
+	Role        UserRole  `json:"role"`        // Admin, Staff, etc
 	Profile_URL string    `json:"profile_url"` // profile picture url to file
 	Created_At  time.Time `json:"created_at"`
 	Updated_At  time.Time `json:"updated_at"`
