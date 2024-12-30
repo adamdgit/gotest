63a80b36c0f046a4f9fa5c13cfeda38ed2b6522f
adamdgit <adam_d@outlook.com>
2024-12-30 16:42:37 +1100

    pass db to route and use fiber handler

--- backend/handlers/getAllPosts.go
+++ backend/handlers/getAllPosts.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"database/sql"
 	"log"
 
 	"github.com/adamdgit/gotest/backend/models"
@@ -8,31 +9,33 @@ import (
 )
 
 // get all posts
-func GetAllPosts(c *fiber.Ctx) error {
-	stmt := "SELECT * FROM posts"
+func GetAllPosts(db *sql.DB) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		stmt := "SELECT * FROM posts"
 
-	rows, err := db.Query(stmt)
-	if err != nil {
-		log.Printf("Error: %s", err)
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"error": "Error retrieving from database",
-		})
-	}
-	defer rows.Close()
-
-	var posts []models.Post
-	for rows.Next() {
-		var post models.Post
-
-		err := rows.Scan(&post.ID, &post.Title, &post.Content, &post.Created_At, &post.Updated_At)
+		rows, err := db.Query(stmt)
 		if err != nil {
 			log.Printf("Error: %s", err)
 			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
 				"error": "Error retrieving from database",
 			})
 		}
-		posts = append(posts, post)
-	}
+		defer rows.Close()
 
-	return c.JSON(posts)
+		var posts []models.Post
+		for rows.Next() {
+			var post models.Post
+
+			err := rows.Scan(&post.ID, &post.Title, &post.Content, &post.Created_At, &post.Updated_At)
+			if err != nil {
+				log.Printf("Error: %s", err)
+				return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+					"error": "Error retrieving from database",
+				})
+			}
+			posts = append(posts, post)
+		}
+
+		return c.JSON(posts)
+	}
 }
--- backend/handlers/getPostById.go
+++ backend/handlers/getPostById.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"database/sql"
 	"log"
 
 	"github.com/adamdgit/gotest/backend/models"
@@ -8,29 +9,31 @@ import (
 )
 
 // Get post by provided id
-func GetPostById(c *fiber.Ctx) error {
-	id := c.Params("id")
+func GetPostById(db *sql.DB) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		id := c.Params("id")
 
-	stmt := "SELECT * FROM posts WHERE post.id = ?"
+		stmt := "SELECT * FROM posts WHERE post.id = ?"
 
-	row, err := db.Query(stmt, id)
-	if err != nil {
-		log.Printf("Error: %s", err)
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"error": "Error retrieving from database",
-		})
+		row, err := db.Query(stmt, id)
+		if err != nil {
+			log.Printf("Error: %s", err)
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": "Error retrieving from database",
+			})
+		}
+		defer row.Close()
+
+		var post models.Post
+
+		err = row.Scan(&post.ID, &post.Title, &post.Content, &post.Created_At, &post.Updated_At)
+		if err != nil {
+			log.Printf("Error: %s", err)
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": "Error retrieving from database",
+			})
+		}
+
+		return c.JSON(post)
 	}
-	defer row.Close()
-
-	var post models.Post
-
-	err = row.Scan(&post.ID, &post.Title, &post.Content, &post.Created_At, &post.Updated_At)
-	if err != nil {
-		log.Printf("Error: %s", err)
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"error": "Error retrieving from database",
-		})
-	}
-
-	return c.JSON(post)
 }
