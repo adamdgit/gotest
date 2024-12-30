02f11c0931c648ad24b09d4c0202939a9ef2851d
adamdgit <adam_d@outlook.com>
2024-12-30 16:45:11 +1100

    Added login route with API fetch

---
+++ frontend/src/components/Routes/Login.tsx
@@ -0,0 +1,37 @@
+import { FormEvent, useState } from 'react'
+
+export default function Login() {
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+
+  async function login(e:FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("username", username)
+    formData.append("password", password)
+
+    const res = await fetch('http://localhost:8081/api/auth/login', {
+      method: "POST",
+      body: formData
+    })
+
+    if (res.ok) {
+      const data = await res.json();
+      console.log(data)
+    }
+  }
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={(e) => login(e)}>
+        <label htmlFor='username'>Username</label>
+        <input type='text' name='username' onChange={(e) => setUsername(e.target.value)} />
+        <label htmlFor='password'>Password</label>
+        <input type='text' name='password' onChange={(e) => setPassword(e.target.value)} />
+        <button type='submit'>Submit</button>
+      </form>
+    </div>
+  )
+}
