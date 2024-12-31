ff8faf7f76bf701c25fa1c7c5f1ee6667508ce2b
adamdgit <adam_d@outlook.com>
2024-12-31 16:36:54 +1100

    Updated JWT auth config

--- backend/middleware/jwt.go
+++ backend/middleware/jwt.go
@@ -1,6 +1,8 @@
 package middleware
 
 import (
+	"log"
+
 	jwtware "github.com/gofiber/contrib/jwt"
 	"github.com/gofiber/fiber/v2"
 )
@@ -8,6 +10,13 @@ import (
 // Middleware JWT function
 func NewAuthMiddleware(secret string) fiber.Handler {
 	return jwtware.New(jwtware.Config{
-		SigningKey: jwtware.SigningKey{Key: []byte(secret)},
+		TokenLookup: "cookie:auth_token",
+		SigningKey:  jwtware.SigningKey{Key: []byte(secret)},
+		ErrorHandler: func(c *fiber.Ctx, err error) error {
+			log.Printf("ERR: %s", err)
+			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+				"error": "Unauthorized",
+			})
+		},
 	})
 }
