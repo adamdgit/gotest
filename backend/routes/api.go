7d1396200867c8dc3a1fd6a94d0d0f5b01d4a1ba
adamdgit <adam_d@outlook.com>
2024-12-31 16:37:08 +1100

    Testing auth config for logout route

--- backend/routes/api.go
+++ backend/routes/api.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 
 	"github.com/adamdgit/gotest/backend/handlers"
+	"github.com/adamdgit/gotest/backend/middleware"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -15,8 +16,8 @@ func RegisterAPIRoutes(app *fiber.App, db *sql.DB) {
 	// }
 
 	// secret := os.Getenv("JWT_SECRET")
-	// jwt := middleware.NewAuthMiddleware(secret)
-	// NOTE: Add jwt to routes that need auth protection
+	authRequired := middleware.NewAuthMiddleware("secret")
+	// NOTE: Add authRequired to routes that need auth protection
 
 	// Post Routes
 	app.Get("/api/v1/posts", handlers.GetAllPosts(db))
@@ -24,6 +25,7 @@ func RegisterAPIRoutes(app *fiber.App, db *sql.DB) {
 
 	// Login & Register routes
 	app.Post("/api/auth/login", handlers.Login(db))
+	app.Post("/api/auth/logout", authRequired, handlers.Logout) // clears session cookie, no db required
 	app.Post("/api/auth/register", handlers.Register(db))
 
 }
