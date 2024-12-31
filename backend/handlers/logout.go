97b6d277d8f1a239596a959ded97a4bfcba9128a
adamdgit <adam_d@outlook.com>
2024-12-31 16:36:40 +1100

    Added logout route, removes http cookie

---
+++ backend/handlers/logout.go
@@ -0,0 +1,20 @@
+package handlers
+
+import (
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gofiber/fiber/v2"
+)
+
+// Deletes http-only cookie
+func Logout(c *fiber.Ctx) error {
+	c.Cookie(&fiber.Cookie{
+		Name:     "auth_token",
+		Value:    "",
+		HTTPOnly: true,
+		Secure:   true,
+		Expires:  time.Now().Add(-time.Hour),
+	})
+	return c.SendStatus(fiber.StatusOK)
+}
