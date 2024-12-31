4e9a1462cef5771ca8e9516eb3cfdce016550971
adamdgit <adam_d@outlook.com>
2024-12-31 16:36:27 +1100

    Changed formdata to body parse JSON

--- backend/handlers/login.go
+++ backend/handlers/login.go
@@ -18,21 +18,37 @@ func CheckPasswordHash(password string, hash string) bool {
 	return err == nil
 }
 
+// JSON format from login body request
+type LoginJSON struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
 func Login(db *sql.DB) fiber.Handler {
 	return func(c *fiber.Ctx) error {
-		// Get form data
-		username := c.FormValue("username")
-		password := c.FormValue("password")
+		var req LoginJSON
+
+		// Parse body JSON and extract username, password
+		err := c.BodyParser(&req)
+		if err != nil {
+			log.Printf("Error: %s", err)
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": "Invalid request body",
+			})
+		}
+
+		username := req.Username
+		password := req.Password
 
 		// Get username and password from DB
-		stmt := "SELECT username, password FROM users WHERE username = ?"
+		stmt := "SELECT ID, username, password FROM users WHERE username = ?"
 		row := db.QueryRowContext(context.Background(), stmt, username)
 
 		var user models.User
 
 		// If ErrNoRows user has provided invalid login details
 		// else we need to check password is valid
-		err := row.Scan(&user.Username, &user.Password)
+		err = row.Scan(&user.ID, &user.Username, &user.Password)
 		if err == sql.ErrNoRows {
 			log.Printf("Error: %s", err)
 			return c.Status(fiber.StatusConflict).JSON(fiber.Map{
@@ -53,7 +69,7 @@ func Login(db *sql.DB) fiber.Handler {
 			"ID":       user.ID,
 			"username": username,
 			"admin":    false,
-			"exp":      time.Now().Add(time.Hour * 72).Unix(),
+			"exp":      time.Now().Add(time.Hour * 12).Unix(),
 		}
 
 		// Create token
@@ -62,9 +78,23 @@ func Login(db *sql.DB) fiber.Handler {
 		// Generate encoded token and send it as response.
 		t, err := token.SignedString([]byte("secret"))
 		if err != nil {
+			log.Printf("Error: %s", err)
 			return c.SendStatus(fiber.StatusInternalServerError)
 		}
 
-		return c.JSON(fiber.Map{"token": t})
+		// Set the token in an HTTP-only cookie
+		c.Cookie(&fiber.Cookie{
+			Name:     "auth_token",
+			Value:    t,
+			HTTPOnly: true,
+			// Secure:   true, // Ensure this is true in production (HTTPS)
+			SameSite: "Strict",
+			Path:     "/",
+			Expires:  time.Now().Add(time.Hour * 12),
+		})
+
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{
+			"message": "Logged in successfully",
+		})
 	}
 }
