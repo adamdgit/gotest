92bea36e754ac28272fcf0edfb839fd326a461c6
adamdgit <adam_d@outlook.com>
2025-01-02 16:33:02 +1100

    Also delete refresh token on logout

--- backend/handlers/logout.go
+++ backend/handlers/logout.go
@@ -16,5 +16,14 @@ func Logout(c *fiber.Ctx) error {
 		Secure:   true,
 		Expires:  time.Now().Add(-time.Hour),
 	})
+
+	c.Cookie(&fiber.Cookie{
+		Name:     "refresh_token",
+		Value:    "",
+		HTTPOnly: true,
+		Secure:   true,
+		Expires:  time.Now().Add(-time.Hour),
+	})
+
 	return c.SendStatus(fiber.StatusOK)
 }
