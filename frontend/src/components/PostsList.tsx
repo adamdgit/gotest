b15fbbf4af56edb5e822bfa166d96cae9b431ace
adamdgit <adam_d@outlook.com>
2025-01-02 15:58:45 +1100

    Moved fetch posts hook to inside component

--- frontend/src/components/PostsList.tsx
+++ frontend/src/components/PostsList.tsx
@@ -1,9 +1,36 @@
-import { useEffect } from 'react'
-import { useFetch } from '../api/useFetch';
+import { useEffect, useState } from 'react'
 
+type Post = {
+  id: number,
+  title: string,
+  content: string,
+  created_at: number,
+  updated_at: number
+}
 
 export default function PostsList() {
-  const { data, loading, error, fetchPosts } = useFetch('http://localhost:8081/api/v1/posts');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const [data, setData] = useState<Post[] | []>([]);
+
+  const fetchPosts = async () => {
+    setLoading(true);
+    setError(null);
+    try {
+      const res = await fetch('http://localhost:8081/api/v1/posts', {
+        credentials: "include"
+      });
+      if (!res.ok) {
+        throw new Error(`HTTP error! status: ${res.status}`);
+      }
+      const result = await res.json();
+      setData(result);
+    } catch (err) {
+      setError((err as Error).message || 'An error occurred');
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
     fetchPosts();
@@ -20,7 +47,15 @@ export default function PostsList() {
   return (
     <ul>
       {data?.map(post => 
-        <li key={post.id}>{post.title} | {post.content}</li>
+        <li key={post.id}>
+          <h2>{post.title}</h2>
+          <p>{post.content}</p>
+          <span>{
+            new Date(post.created_at)
+              .toLocaleDateString("en-au", { minute: '2-digit', hour: '2-digit' })
+          }
+          </span>
+        </li>
       )}
     </ul>
   )
