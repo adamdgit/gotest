3bb921a0fb2261c50d7447a681d8a05cd930c3ad
adamdgit <adam_d@outlook.com>
2025-01-02 15:58:08 +1100

    Changed log text

--- backend/middleware/jwt.go
+++ backend/middleware/jwt.go
@@ -13,7 +13,7 @@ func NewAuthMiddleware(secret string) fiber.Handler {
 		TokenLookup: "cookie:auth_token",
 		SigningKey:  jwtware.SigningKey{Key: []byte(secret)},
 		ErrorHandler: func(c *fiber.Ctx, err error) error {
-			log.Printf("ERR: %s", err)
+			log.Printf("Error: %s", err)
 			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
 				"error": "Unauthorized",
 			})
