1040c3bf8a586bf2f86e53eb6bbb5bb8594af637
adamdgit <adam_d@outlook.com>
2025-01-04 21:01:05 +1100

    added logs, testing auth roles

--- backend/handlers/login.go
+++ backend/handlers/login.go
@@ -3,6 +3,8 @@ package handlers
 import (
 	"context"
 	"database/sql"
+	"encoding/gob"
+	"log"
 
 	"github.com/adamdgit/gotest/backend/models"
 	_ "github.com/go-sql-driver/mysql"
@@ -33,16 +35,17 @@ func Login(db *sql.DB, store *session.Store) fiber.Handler {
 		password := req.Password
 
 		// Get username and password from DB
-		stmt := "SELECT ID, username, password FROM users WHERE username = ?"
+		stmt := "SELECT ID, username, password, role FROM users WHERE username = ?"
 		row := db.QueryRowContext(context.Background(), stmt, username)
 
 		var user models.User
 
 		// If ErrNoRows user has provided invalid login details
 		// else we need to check password is valid
-		err = row.Scan(&user.ID, &user.Username, &user.Password)
+		err = row.Scan(&user.ID, &user.Username, &user.Password, &user.Role)
 		if err == sql.ErrNoRows {
-			return c.Status(fiber.StatusConflict).JSON(fiber.Map{
+			log.Printf("---error: %s", err)
+			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
 				"message": "Invalid Login Credentials",
 			})
 		}
@@ -51,21 +54,29 @@ func Login(db *sql.DB, store *session.Store) fiber.Handler {
 		hash := user.Password
 		match := CheckPasswordHash(password, hash)
 		if !match {
-			return c.SendStatus(fiber.StatusUnauthorized)
+			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+				"message": "Invalid Login Credentials",
+			})
 		}
 
 		// Create session cookie
 		session, err := store.Get(c)
 		if err != nil {
+			log.Printf("---error: %s", err)
 			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
 				"message": "Error creating session",
 			})
 		}
 
+		// FIX: gob encoder error when reading models.UserRole
+		gob.Register(models.UserRole(""))
+
 		// Set session id and username
 		session.Set("id", user.ID)
 		session.Set("username", user.Username)
+		session.Set("role", user.Role)
 		if err := session.Save(); err != nil {
+			log.Printf("---error: %s", err)
 			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
 				"message": "Error creating session",
 			})
