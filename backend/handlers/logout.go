239601c7a0876ada65425a94b705c8a2da3cb498
adamdgit <adammdemol@outlook.com>
2025-01-31 16:24:51 +1100

    Set session to null on logout and return json success

--- backend/handlers/logout.go
+++ backend/handlers/logout.go
@@ -1,18 +1,33 @@
 package handlers
 
 import (
+	"database/sql"
+
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/session"
 )
 
 // Deletes http-only cookie
-func Logout(store *session.Store) fiber.Handler {
+func Logout(db *sql.DB, store *session.Store) fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		// Retrieve session cookie if it exists
 		session, err := store.Get(c)
 		if err != nil {
 			return c.SendStatus(fiber.StatusInternalServerError)
+		}
+
+		user_id := session.Get("user_id")
+		// handles null types for db
+		var nullSessionID sql.NullString
+		nullSessionID.Valid = false
+
+		// Remove session from user in db
+		_, err = db.Exec("UPDATE users SET session_id = ? WHERE id = ?", nullSessionID, user_id)
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": "Error logging out",
+			})
 		}
 
 		// Destroy session
@@ -22,6 +37,8 @@ func Logout(store *session.Store) fiber.Handler {
 			})
 		}
 
-		return c.SendStatus(fiber.StatusOK)
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{
+			"message": "Logged out successfully",
+		})
 	}
 }
