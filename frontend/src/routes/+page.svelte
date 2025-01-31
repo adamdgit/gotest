05f4e8fe9a1512743f7e136ce1479a7494c66159
adamdgit <adammdemol@outlook.com>
2025-01-31 16:21:05 +1100

    Removed frontend

--- frontend/src/routes/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-  let { data } = $props();
-</script>
-
-<h1>Home</h1>
-<p>Logged in, protected route</p>
-
-<p>Hello {data.user}</p>
--- frontend/src/routes/login/+page.server.ts
+++
@@ -1,6 +0,0 @@
-import type { PageServerLoad } from './$types';
-
-export const load: PageServerLoad = async () => {
-  // Simulate fetching data from an API or database
-  return { title: 'SSR Example', content: 'This page is server-side rendered.' };
-};
