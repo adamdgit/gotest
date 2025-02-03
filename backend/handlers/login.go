f7e328fa1cd6aeab8a39c48368538d57d79fa6a9
adamdgit <adammdemol@outlook.com>
2025-02-03 16:32:17 +1100

    testing moving error handling to function to reduce code replication

--- backend/handlers/login.go
+++ backend/handlers/login.go
@@ -3,10 +3,10 @@ package handlers
 import (
 	"context"
 	"database/sql"
-	"log"
 	"strconv"
 
 	"github.com/adamdgit/gotest/backend/models"
+	"github.com/adamdgit/gotest/backend/utils"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/session"
@@ -25,30 +25,21 @@ func Login(db *sql.DB, store *session.Store) fiber.Handler {
 
 		// Parse body JSON and extract email, password
 		err := c.BodyParser(&req)
-		if err != nil {
-			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-				"error": "Invalid request body",
-			})
-		}
+		utils.HandleError(c, err, "invalid request body")
 
 		email := req.Email
 		password := req.Password
 
 		// Get email and password from DB
-		stmt := "SELECT ID, email, password FROM users WHERE email = ?"
+		stmt := "SELECT ID, email, password, profile_url FROM users WHERE email = ?"
 		row := db.QueryRowContext(context.Background(), stmt, email)
 
 		var user models.User
 
 		// If ErrNoRows user has provided invalid login details
 		// else we need to check password is valid
-		err = row.Scan(&user.ID, &user.Email, &user.Password)
-		if err == sql.ErrNoRows {
-			log.Printf("---error: %s", err)
-			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-				"message": "Invalid Login Credentials",
-			})
-		}
+		err = row.Scan(&user.ID, &user.Email, &user.Password, &user.Profile_URL)
+		utils.HandleError(c, err, "invalid login credentials")
 
 		// Check password matches the hash
 		hash := user.Password
@@ -61,36 +52,23 @@ func Login(db *sql.DB, store *session.Store) fiber.Handler {
 
 		// Create session cookie
 		session, err := store.Get(c)
-		if err != nil {
-			log.Printf("---error: %s", err)
-			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-				"message": "Error creating session",
-			})
-		}
+		utils.HandleError(c, err, "Error creating session")
 
 		// store userID in session, used for validating user in other routes
 		session.Set("user_id", strconv.Itoa(user.ID))
-
 		var session_id = session.ID()
 
-		if err := session.Save(); err != nil {
-			log.Printf("Error saving session: %v", err)
-			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-				"error": "Error creating session",
-			})
-		}
+		err = session.Save()
+		utils.HandleError(c, err, "Error creating session")
 
 		// Set session id in users db
 		_, err = db.Exec("UPDATE users SET session_id = ? WHERE id = ?", session_id, user.ID)
-		if err != nil {
-			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-				"error": "Error creating session",
-			})
-		}
+		utils.HandleError(c, err, "Error creating session")
 
 		return c.Status(fiber.StatusOK).JSON(fiber.Map{
-			"message": "Logged in successfully",
-			"data":    fiber.Map{"email": user.Email, "session_id": session_id},
+			"message":     "Logged in successfully",
+			"email":       user.Email,
+			"profile_url": user.Profile_URL,
 		})
 	}
 }
