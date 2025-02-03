b41a50a03542ced0491856f08bc35e1ad87e56ad
adamdgit <adammdemol@outlook.com>
2025-02-03 19:42:58 +1100

    Updated posts endpoints to products

--- backend/routes/api.go
+++ backend/routes/api.go
@@ -4,7 +4,6 @@ import (
 	"database/sql"
 
 	"github.com/adamdgit/gotest/backend/handlers"
-	"github.com/adamdgit/gotest/backend/middleware"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/session"
 )
@@ -13,8 +12,8 @@ func RegisterAPIRoutes(app *fiber.App, db *sql.DB, store *session.Store) {
 	// NOTE: Protect routes by adding AuthLoggedIn or AuthIsAdmin
 	// AuthLoggedIn, users must be logged in to access API
 	// AuthIsAdmin, users must have admin role to access API
-	app.Get("/api/v1/posts", middleware.AuthIsAdmin(db, store), handlers.GetAllPosts(db))
-	app.Get("/api/v1/posts/:id", handlers.GetPostById(db))
+	app.Get("/api/v1/products", handlers.GetProductList(db))
+	app.Get("/api/v1/product/:id", handlers.GetProduct(db))
 
 	// Login, Logout, Register
 	app.Post("/api/auth/login", handlers.Login(db, store))
