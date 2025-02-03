abde7c62982b393478342bf029ad92cd6ec50047
adamdgit <adammdemol@outlook.com>
2025-02-03 19:47:26 +1100

    header layout for every page

---
+++ frontend/src/pages/Header.tsx
@@ -0,0 +1,32 @@
+import { useAuth } from "../AuthProvider"
+
+export default function Header(props) {
+
+    const { userData } = useAuth();
+
+    return (
+      <>
+          <header>
+            <h3>Appname</h3>
+            {userData.email ? 
+              <>
+                <nav class="nabvar">
+                    <ul class="navlist">
+                        <li><a href="#">Admin</a></li>
+                        <li><a href="#">Forms</a></li>
+                        <li><a href="#">Staff</a></li>
+                        <li><a href="/inventory">Inventory</a></li>
+                    </ul>
+                </nav>
+                <div class="profilewrapper">
+                  {userData?.email}
+                  <button class='btn'>Logout</button>
+                </div>
+            </>
+              : null
+            }
+          </header>
+          {props.children}
+      </>
+    )
+}
