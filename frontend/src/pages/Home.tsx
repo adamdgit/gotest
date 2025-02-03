f1e86852ba9bc1b5fb979da745774fedf8006f65
adamdgit <adammdemol@outlook.com>
2025-02-03 19:47:39 +1100

    moved header to layout

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,30 +1,17 @@
 import { useAuth } from '../AuthProvider';
+import { useNavigate } from '@solidjs/router';
 
 export default function Home() {
   const { userData } = useAuth();
+  const navigate = useNavigate();
+
+  if (!userData.email) {
+    navigate("/");
+  }
 
   return (
-    <>
-      <header>
-          <nav class="nabvar">
-              <ul class="navlist">
-                  <li><a href="/home">Home</a></li>
-                  <li><a href="/">Login</a></li>
-                  <li><a href="/register">Register</a></li>
-              </ul>
-          </nav>
-          <div class="profilewrapper">
-            {userData?.email ?? "guest"}
-            {userData ? (
-                <button class='btn'>Logout</button>
-              ) : (
-              <a href="/" class='btn'>Login</a>
-            )}
-          </div>
-      </header>
-      <main>
-        Home page
-      </main>
-    </>
+    <main>
+      Home page
+    </main>
   )
 }
