e5464d1280203fa9c1925a965e9784a63578a16e
adamdgit <adammdemol@outlook.com>
2025-02-03 19:45:59 +1100

    added inventory route

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,13 @@
 import { render } from "solid-js/web";
 import { Router, Route } from "@solidjs/router";
-import "./styles/index.css"
+import { AuthProvider } from "./AuthProvider";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
-import { AuthProvider } from "./AuthProvider";
 import Register from "./pages/Register";
+import Inventory from "./pages/Inventory";
+import "./styles/index.css"
+import Header from "./pages/Header";
 
 const root = document.getElementById("root");
 if (!root) throw new Error("Somehow root doesn't exist?")
@@ -13,10 +15,11 @@ if (!root) throw new Error("Somehow root doesn't exist?")
 render(
   () => (
     <AuthProvider>
-      <Router>
-        <Route path="/" component={Login} />
+      <Router root={Header}>
+        <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
         <Route path="/home" component={Home} />
+        <Route path="/inventory" component={Inventory} />
         <Route path="*paramName" component={NotFound} />
       </Router>
     </AuthProvider>
