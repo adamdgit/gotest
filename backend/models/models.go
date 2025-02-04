8c0446fed4f6c892c8538e065371d1c5613fed26
adamdgit <adammdemol@outlook.com>
2025-02-04 16:33:59 +1100

    removed as unnecesary

--- backend/models/models.go
+++ backend/models/models.go
@@ -22,7 +22,6 @@ type User struct {
 	Phone       string    `json:"phone"` // format: +61 000 000 000
 	Role        UserRole  `json:"role"`
 	Profile_URL string    `json:"profile_url"` // profile picture url to file
-	Session_ID  string    `json:"session_id"`
 	Last_Login  time.Time `json:"last_login"`
 	Created_At  time.Time `json:"created_at"`
 	Updated_At  time.Time `json:"updated_at"`
