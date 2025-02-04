bad21ff173ee9eee80109f884d359b1b79dfd7f4
adamdgit <adammdemol@outlook.com>
2025-02-04 16:36:11 +1100

    moved login check to header

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,13 +1,7 @@
 import { useAuth } from '../AuthProvider';
-import { useNavigate } from '@solidjs/router';
 
 export default function Home() {
   const { userData } = useAuth();
-  const navigate = useNavigate();
-
-  if (!userData.email) {
-    navigate("/");
-  }
 
   return (
     <main>
