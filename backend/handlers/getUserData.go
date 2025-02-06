f1f25d5b1902b364d0368df816d93adba1aaaa49
adamdgit <adammdemol@outlook.com>
2025-02-06 17:03:29 +1100

    fixed userdata fetch

--- backend/handlers/getUserData.go
+++ backend/handlers/getUserData.go
@@ -1,37 +1,40 @@
 package handlers
 
 import (
+	"context"
 	"database/sql"
-	"log"
 
+	"github.com/adamdgit/gotest/backend/models"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gofiber/fiber/v2"
 )
 
-// Deletes http-only cookie
+// Get user data from sessions_id
 func GetUserData(db *sql.DB) fiber.Handler {
 	return func(c *fiber.Ctx) error {
-		log.Printf("context: %s", c.Request().Header.Header())
-		// Retrieve the session if it exists
 		sessionID := c.Cookies("access_token")
 
-		if sessionID == "" {
+		// Get the user_id via session_id
+		row := db.QueryRowContext(context.Background(),
+			"SELECT u.email, u.role, u.profile_url FROM sessions s JOIN users u ON s.user_id = u.id WHERE s.session_id = ?",
+			sessionID)
+
+		var user models.User
+
+		err := row.Scan(&user.Email, &user.Role, &user.Profile_URL)
+		if err == sql.ErrNoRows {
 			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
 				"error": "Couldn't retrieve user data",
 			})
 		}
 
-		// TODO; select user_id from sessions table
-		// Select user data from user table
-		// Return data for user
-		return nil
 		// Success, return data as json
-		// return c.Status(fiber.StatusOK).JSON(fiber.Map{
-		// 	"user": fiber.Map{
-		// 		"email":       user.Email,
-		// 		"role":        user.Role,
-		// 		"profile_url": user.Profile_URL,
-		// 	},
-		// })
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{
+			"user": fiber.Map{
+				"email":       user.Email,
+				"role":        user.Role,
+				"profile_url": user.Profile_URL,
+			},
+		})
 	}
 }
