2f9bad6cfc102ffe526b84fc03cd043d8cac352a
adamdgit <adammdemol@outlook.com>
2025-02-17 18:08:46 +1100

    Updated inventory page

---
+++ frontend/src/pages/inventory/Inventory.tsx
@@ -0,0 +1,24 @@
+import { createSignal, Show } from "solid-js";
+import { useAuth } from "../../AuthProvider";
+import { Product } from "../../types/types";
+import CategoryList from "./CategorySelect";
+import "../../styles/Products.css";
+
+export default function Inventory() {
+    const { userData } = useAuth();
+
+    const [products, setProducts] = createSignal<Product[]>([]);
+    const [loading, setLoading] = createSignal(false);
+    const [error, setError] = createSignal(false);
+
+    return (
+        <main>
+            <h1>Inventory Management</h1>
+            <Show when={error()}>
+                <p style={{color: 'red'}}>Something went wrong</p> 
+            </Show>
+
+            <CategoryList />
+        </main>
+    )
+}
