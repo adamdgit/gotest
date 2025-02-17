477ab9f32b43909e46dd7c0fe839109fd3604067
adamdgit <adammdemol@outlook.com>
2025-02-17 18:07:35 +1100

    Added more models for product and inventory management

--- backend/models/models.go
+++ backend/models/models.go
@@ -7,10 +7,11 @@ import (
 type UserRole string
 
 const (
-	Admin   UserRole = "admin"
-	Manager UserRole = "manager"
-	Staff   UserRole = "staff"
-	Member  UserRole = "member"
+	Admin    UserRole = "admin"
+	Manager  UserRole = "manager"
+	Staff    UserRole = "staff"
+	Member   UserRole = "member"
+	Customer UserRole = "customer"
 )
 
 type User struct {
@@ -20,6 +21,7 @@ type User struct {
 	Firstname   string   `json:"firstname"`
 	Lastname    string   `json:"lastname"`
 	Phone       string   `json:"phone"` // format: +61 000 000 000
+	Address     string   `json:"address"`
 	Role        UserRole `json:"role"`
 	Profile_URL string   `json:"profile_url"` // profile picture url to file
 	// TwoFac_Email string    `json:"twofac_email"`
@@ -27,17 +29,6 @@ type User struct {
 	Last_Login time.Time `json:"last_login"`
 	Created_At time.Time `json:"created_at"`
 	Updated_At time.Time `json:"updated_at"`
-}
-
-// Inventory products
-type Product struct {
-	ID          int       `json:"id"`
-	Name        string    `json:"name"`
-	Brand       string    `json:"brand"`
-	Description string    `json:"description"`
-	Price       string    `json:"price"` // Decimal(10,2) in MySQL can convert from string to float
-	Created_At  time.Time `json:"created_at"`
-	Updated_At  time.Time `json:"updated_at"`
 }
 
 // Valid Session stores
@@ -49,7 +40,48 @@ type Session struct {
 	Session_Expires string    `json:"session_expires"`
 	Refresh_Expires string    `json:"refresh_expires"`
 	IP_Address      string    `json:"ip_address"`
-	User_Agent      string    `json:"user_agent"`
 	Created_At      time.Time `json:"created_at"` // Initial login date
 	Updated_At      time.Time `json:"updated_at"` // most recent session refresh
+}
+
+// Inventory products
+type Product struct {
+	ID          int       `json:"id"`
+	Name        string    `json:"name"`
+	Brand       string    `json:"brand"`
+	Description string    `json:"description"`
+	Price       string    `json:"price"` // Decimal(10,2) in MySQL can convert from string to float
+	Count       int       `json:"count"`
+	Category    int       `json:"category"`
+	Created_At  time.Time `json:"created_at"`
+	Updated_At  time.Time `json:"updated_at"`
+}
+
+// PRoduct categories
+type Categories struct {
+	ID          int       `json:"id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	Created_At  time.Time `json:"created_at"`
+	Updated_At  time.Time `json:"updated_at"`
+}
+
+// Product supplier info
+type Suppliers struct {
+	ID         int       `json:"id"`
+	Name       string    `json:"name"`
+	Contact    string    `json:"contact"`
+	Email      string    `json:"email"`
+	Phone      string    `json:"phone"`
+	Address    string    `json:"address"`
+	Created_At time.Time `json:"created_at"`
+	Updated_At time.Time `json:"updated_at"`
+}
+
+// WIP: Not yet implemented
+// Each user can have many devices
+type Devices struct {
+	ID        int    `json:"id"`
+	User_ID   int    `json:"user_id"`
+	Device_ID string `json:"device_id"`
 }
