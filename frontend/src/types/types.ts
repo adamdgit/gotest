22ed3ace641d694ce41fcd7e78454f58f07ed078
adamdgit <adammdemol@outlook.com>
2025-02-17 18:10:31 +1100

    moved types to a type file

---
+++ frontend/src/types/types.ts
@@ -0,0 +1,13 @@
+export type Product = {
+    id: number,
+    name: string,
+    brand: string, 
+    description: string,
+    price: number,
+}
+
+export type Category = {
+    id: number,
+    name: string,
+    description: string
+}
