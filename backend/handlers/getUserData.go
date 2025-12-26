ffbfd5b99693768f916333c7b72e26e585080659
adamdgit <adammdemol@outlook.com>
2025-12-26 17:17:12 +1100

    Fixed api annotations

--- backend/handlers/getUserData.go
+++ backend/handlers/getUserData.go
@@ -16,8 +16,8 @@ import (
 // @Description Returns authenticated user data
 // @Tags user
 // @Produce json
-// @Success 200 {object} models.UserDataRes
-// @Failure 401 {object} map[string]string "Unauthorized"
+// @Success 200 {object} models.UserDataResponse
+// @Failure 401 {object} models.ErrorResponse "Internal server error"
 // @Router /user [get]
 func GetUserData(db *sql.DB) fiber.Handler {
 	return func(c *fiber.Ctx) error {
