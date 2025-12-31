88e54987a7af1840412d71328910fe69ddf0e6e9
adamdgit <adammdemol@outlook.com>
2025-12-31 18:05:45 +1100

    Updated api response types

--- backend/api/apiResponses.go
+++ backend/api/apiResponses.go
@@ -1,9 +1,36 @@
 package api
 
-import "github.com/adamdgit/gotest/backend/models"
+import (
+	"time"
 
-type UserData struct {
+	"github.com/adamdgit/gotest/backend/models"
+)
+
+type UserDataLogin struct {
 	Email       string          `json:"email"`
 	Role        models.UserRole `json:"role"`
 	Profile_URL string          `json:"profile_url"`
+}
+
+type UserDataSmall struct {
+	ID        int             `json:"id"`
+	Email     string          `json:"email"`
+	Firstname string          `json:"firstname"`
+	Lastname  string          `json:"lastname"`
+	Address   string          `json:"address"`
+	Role      models.UserRole `json:"role"`
+}
+
+// Get all user data, for admins only
+type UserDataComplete struct {
+	ID          int             `json:"id"`
+	Email       string          `json:"email"`
+	Firstname   string          `json:"firstname"`
+	Lastname    string          `json:"lastname"`
+	Phone       string          `json:"phone"`
+	Address     string          `json:"address"`
+	Role        models.UserRole `json:"role"`
+	Profile_URL string          `json:"profile_url"`
+	Created_At  time.Time       `json:"created_at"`
+	Updated_At  time.Time       `json:"updated_at"`
 }
