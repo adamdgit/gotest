ed53dda77ea5e2a4ca0c65cc3ffe2a563643d3bc
adamdgit <adammdemol@outlook.com>
2025-12-31 18:06:21 +1100

    Fixed email exists logic, added profile url

--- backend/handlers/register.go
+++ backend/handlers/register.go
@@ -1,9 +1,10 @@
 package handlers
 
 import (
-	"context"
 	"database/sql"
+	"log"
 
+	"github.com/adamdgit/gotest/backend/api"
 	"github.com/gofiber/fiber/v2"
 	"golang.org/x/crypto/bcrypt"
 )
@@ -26,38 +27,48 @@ func Register(db *sql.DB) fiber.Handler {
 		// Parse body JSON and extract email, password
 		err := c.BodyParser(&req)
 		if err != nil {
-			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-				"error": "Invalid request body",
-			})
+			return c.Status(fiber.StatusBadRequest).JSON(
+				api.ErrInvalidBody,
+			)
 		}
 
 		email := req.Email
 		password := req.Password
+		// Frontend folder for assets
+		profile_url := "$lib/assets/profiledefault.svg"
 
+		var exists bool
 		// Check if user exists already. before creating
-		rowUserExists := db.QueryRowContext(context.Background(),
-			"SELECT email FROM users WHERE email = ?", email)
+		err = db.QueryRow("SELECT EXISTS(SELECT email FROM users WHERE email = ?)", email).Scan(&exists)
+		log.Printf("Register: exists? %t", exists)
+		if exists {
+			return c.Status(fiber.StatusConflict).JSON(
+				api.ErrEmailInUse,
+			)
+		}
 
-		// If ErrNoRows returns then no user exists and we can continue
-		// else we need to return conflict error status
-		err = rowUserExists.Scan(email)
-		if err != sql.ErrNoRows {
-			return c.Status(fiber.StatusConflict).JSON(fiber.Map{
-				"message": "Email already in use",
-			})
+		if err != nil {
+			log.Printf("Register: err? %s", err)
+			return c.Status(fiber.StatusInternalServerError).JSON(
+				api.ErrInternalServer,
+			)
 		}
 
 		// Hash password before inserting to db if email is available
 		hash, err := HashPassword(password)
 		if err != nil {
-			return c.SendStatus(fiber.StatusInternalServerError)
+			return c.Status(fiber.StatusInternalServerError).JSON(
+				api.ErrInternalServer,
+			)
 		}
 
 		// Insert new user into DB
-		row, err := db.Query("INSERT INTO users (email, password) VALUES (?, ?)",
-			email, hash)
+		row, err := db.Query("INSERT INTO users (email, password, profile_url) VALUES (?, ?, ?)",
+			email, hash, profile_url)
 		if err != nil {
-			return c.SendStatus(fiber.StatusInternalServerError)
+			return c.Status(fiber.StatusInternalServerError).JSON(
+				api.ErrInternalServer,
+			)
 		}
 		defer row.Close()
 
