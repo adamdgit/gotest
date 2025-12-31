4f5ac96523569fb07eff0e3c724b08871d4d9e15
adamdgit <adammdemol@outlook.com>
2025-12-31 18:07:46 +1100

    Cleaned up route locations

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -4,6 +4,9 @@ import (
 	"database/sql"
 
 	"github.com/adamdgit/gotest/backend/handlers"
+	"github.com/adamdgit/gotest/backend/handlers/categories"
+	"github.com/adamdgit/gotest/backend/handlers/products"
+	"github.com/adamdgit/gotest/backend/handlers/users"
 	"github.com/adamdgit/gotest/backend/middleware"
 	"github.com/gofiber/fiber/v2"
 	"github.com/oschwald/geoip2-golang"
@@ -13,45 +16,60 @@ func RegisterAPIRoutes(app *fiber.App, db *sql.DB, geoDb *geoip2.Reader) {
 	// Get all Products
 	app.Get("/api/v1/products",
 		middleware.AuthSessionIsValid(db),
-		handlers.GetProductList(db),
+		products.GetProductList(db),
 	)
 	// Get product info by ID
 	app.Get("/api/v1/product/:id",
 		middleware.AuthSessionIsValid(db),
-		handlers.GetProduct(db),
+		products.GetProduct(db),
 	)
 	// Update product by ID
 	app.Put("/api/v1/product/:id",
-		middleware.AuthIsAdmin(db),
-		handlers.UpdateProduct(db),
+		middleware.AuthSessionIsValid(db),
+		products.UpdateProduct(db),
+	)
+	// Get products for selected category
+	app.Get("/api/v1/products/:categoryid",
+		middleware.AuthSessionIsValid(db),
+		products.GetProductsByCategory(db),
 	)
 
 	// Get all categories
 	app.Get("/api/v1/categories",
 		middleware.AuthSessionIsValid(db),
-		handlers.GetCategories(db),
+		categories.GetCategories(db),
 	)
 	// Add new categories
 	app.Put("/api/v1/categories",
 		middleware.AuthSessionIsValid(db),
-		handlers.AddNewCategory(db),
+		categories.AddNewCategory(db),
 	)
-	// Get products for selected category
-	app.Get("/api/v1/products/:categoryid",
-		middleware.AuthSessionIsValid(db),
-		handlers.GetProductsByCategory(db),
-	)
+
+	// Refresh session using refresh token
+	app.Get("/api/refresh", handlers.RefreshAccessToken(db))
 
 	// Login, Logout, Register
 	app.Post("/api/auth/login", handlers.Login(db, geoDb))
 	app.Get("/api/auth/logout", handlers.Logout(db))
 	app.Post("/api/auth/register", handlers.Register(db))
 
-	// Gets current users data
+	// Gets users data based on session cookie
 	app.Get("/api/user",
 		middleware.AuthSessionIsValid(db),
-		handlers.GetUserData(db),
+		users.GetUserData(db),
 	)
 
-	app.Get("/api/refresh", handlers.RefreshAccessToken(db))
+	// Get user by ID
+	app.Get("/api/admin/user/:id",
+		middleware.AuthSessionIsValid(db),
+		middleware.AuthUserHasRole(db, "admin"),
+		users.GetUserDataByID(db),
+	)
+
+	// Get all user data by search query
+	app.Get("/api/admin/users",
+		middleware.AuthSessionIsValid(db),
+		middleware.AuthUserHasRole(db, "admin"),
+		users.GetUserByQuery(db),
+	)
 }
