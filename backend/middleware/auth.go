f8c7f1f630e476787654c28da1f97785e00c6997
adamdgit <adammdemol@outlook.com>
2025-12-31 18:07:04 +1100

    Fixed error handling, added user has role auth handler

--- backend/middleware/auth.go
+++ backend/middleware/auth.go
@@ -3,8 +3,11 @@ package middleware
 import (
 	"database/sql"
 	"log"
+	"slices"
 	"time"
 
+	"github.com/adamdgit/gotest/backend/api"
+	"github.com/adamdgit/gotest/backend/models"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -15,8 +18,10 @@ func AuthSessionIsValid(db *sql.DB) fiber.Handler {
 		access_token := c.Cookies("access_token")
 
 		if access_token == "" {
-			log.Printf("error: no access token")
-			return c.SendStatus(fiber.StatusUnauthorized)
+			log.Printf("AuthSessionIsValid ERR: no access token")
+			return c.Status(fiber.StatusUnauthorized).JSON(
+				api.ErrAccessExpired,
+			)
 		}
 
 		var access_expiration time.Time
@@ -24,15 +29,19 @@ func AuthSessionIsValid(db *sql.DB) fiber.Handler {
 		// check token exists in db
 		err := db.QueryRow("SELECT access_expires FROM sessions WHERE access_token = ?", access_token).
 			Scan(&access_expiration)
-		if err == sql.ErrNoRows {
-			log.Printf("error, %s", err)
-			return c.SendStatus(fiber.StatusUnauthorized)
+		if err != nil {
+			log.Printf("AuthSessionIsValid ERR, %s", err)
+			return c.Status(fiber.StatusUnauthorized).JSON(
+				api.ErrAccessExpired,
+			)
 		}
 
 		// check token expiration
 		if time.Now().UTC().After(access_expiration) {
-			log.Printf("error: expired token %s | %s", access_expiration, access_token)
-			return c.SendStatus(fiber.StatusUnauthorized)
+			log.Printf("AuthSessionIsValid ERR: expired token %s | %s", access_expiration, access_token)
+			return c.Status(fiber.StatusUnauthorized).JSON(
+				api.ErrAccessExpired,
+			)
 		}
 
 		// TODO? We can also add checks for things like IP / geolocation
@@ -43,10 +52,37 @@ func AuthSessionIsValid(db *sql.DB) fiber.Handler {
 	}
 }
 
-// Checks User is admin role for protected routes
-func AuthIsAdmin(db *sql.DB) fiber.Handler {
+// AuthIsAdmin middleware ensures the user has admin privileges
+// NOTE: AuthSessionIsValid() should always be run first,
+// so we don't need to check the session is valid again
+func AuthUserHasRole(db *sql.DB, allowedRoles ...models.UserRole) fiber.Handler {
 	return func(c *fiber.Ctx) error {
-		// Retrieve the session if it exists
+		accessToken := c.Cookies("access_token")
+
+		// Retrieve user role
+		var user models.User
+
+		err := db.QueryRow(`
+            SELECT u.id, u.role
+            FROM users u
+            INNER JOIN sessions s ON u.id = s.user_id
+            WHERE s.access_token = ?
+        `, accessToken).Scan(&user.ID, &user.Role)
+		if err != nil {
+			log.Printf("error: get user role %s", err)
+			return c.Status(fiber.StatusInternalServerError).JSON(
+				api.ErrInternalServer,
+			)
+		}
+
+		// If role isn't allowed, throw error
+		if !slices.Contains(allowedRoles, user.Role) {
+			return c.Status(fiber.StatusForbidden).JSON(
+				api.ErrForbidden,
+			)
+		}
+
+		// Role is valid, continue
 		return c.Next()
 	}
 }
