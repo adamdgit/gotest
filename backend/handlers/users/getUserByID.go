45e9b7863a4958642cb0e7869bd395cc14c334d8
adamdgit <adammdemol@outlook.com>
2026-01-10 17:00:04 +1100

    Updated struct

--- backend/handlers/users/getUserByID.go
+++ backend/handlers/users/getUserByID.go
@@ -12,7 +12,7 @@ func GetUserDataByID(db *sql.DB) fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		id := c.Params("userid")
 
-		var user api.UserDataComplete
+		var user api.AdminUserDataRes
 
 		err := db.QueryRow(
 			`SELECT 
@@ -29,7 +29,7 @@ func GetUserDataByID(db *sql.DB) fiber.Handler {
 			FROM users 
 			WHERE id = ?`, id,
 		).Scan(
-			user.ID, user.Email, user.Firstname, user.Lastname, user.Phone, user.Address, user.Profile_URL, user.Created_At, user.Updated_At,
+			user.ID, user.Email, user.Firstname, user.Lastname, user.Phone, user.Address, user.Role, user.Profile_URL, user.Created_At, user.Updated_At,
 		)
 		if err != nil {
 			log.Printf("Error: %s", err)
