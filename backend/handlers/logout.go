9e6da7ffcda171a3940c088bd7a149dea2f9309d
adamdgit <adammdemol@outlook.com>
2026-01-14 17:53:32 +1100

    Updated API err response, decode token string back into rand bytes

--- backend/handlers/logout.go
+++ backend/handlers/logout.go
@@ -2,8 +2,10 @@ package handlers
 
 import (
 	"database/sql"
+	"encoding/base64"
 	"time"
 
+	"github.com/adamdgit/gotest/backend/api"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gofiber/fiber/v2"
 )
@@ -11,20 +13,28 @@ import (
 // Deletes http-only cookie
 func Logout(db *sql.DB) fiber.Handler {
 	return func(c *fiber.Ctx) error {
-		refreshToken := c.Cookies("refresh_token")
+		refresh_token := c.Cookies("refresh_token")
 
-		if refreshToken == "" {
-			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-				"error": "No session found",
-			})
+		if refresh_token == "" {
+			return c.Status(fiber.StatusUnauthorized).JSON(
+				api.ErrInvalidRequest,
+			)
+		}
+
+		// incoming tokens must be decoded into binary (how they are stored in DB)
+		decoded_refresh, err := base64.RawURLEncoding.DecodeString(refresh_token)
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(
+				api.ErrInternalServer,
+			)
 		}
 
 		// Delete session from db
-		_, err := db.Exec("DELETE FROM sessions WHERE refresh_token = ?", refreshToken)
+		_, err = db.Exec("DELETE FROM sessions WHERE refresh_token = ?", decoded_refresh)
 		if err != nil {
-			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-				"error": "Error logging out",
-			})
+			return c.Status(fiber.StatusUnauthorized).JSON(
+				api.ErrInternalServer,
+			)
 		}
 
 		// Delete cookies on logout
@@ -47,7 +57,7 @@ func Logout(db *sql.DB) fiber.Handler {
 		})
 
 		return c.Status(fiber.StatusOK).JSON(fiber.Map{
-			"meessage": "Logged out successfully",
+			"message": "Logged out successfully",
 		})
 	}
 }
