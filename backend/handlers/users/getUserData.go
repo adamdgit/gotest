36d3d854d50f9586cb6f627e445630634f4c4bda
adamdgit <adammdemol@outlook.com>
2026-01-15 23:31:10 +1100

    Code clean up

--- backend/handlers/users/getUserData.go
+++ backend/handlers/users/getUserData.go
@@ -32,6 +32,8 @@ func GetUserData(db *sql.DB) fiber.Handler {
 			)
 		}
 
+		var user models.User
+
 		// Get the user_id via session_id and check its valid
 		row := db.QueryRowContext(context.Background(),
 			`SELECT u.email, u.role, u.profile_url 
@@ -39,8 +41,6 @@ func GetUserData(db *sql.DB) fiber.Handler {
 			JOIN users u ON s.user_id = u.id 
 			WHERE s.access_token = ?`,
 			decoded_access)
-
-		var user models.User
 
 		err = row.Scan(&user.Email, &user.Role, &user.Profile_URL)
 		if err != nil {
