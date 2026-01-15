db8c4dfb0a25ce60562714601d1a3af4cea0af1b
adamdgit <adammdemol@outlook.com>
2026-01-15 17:10:21 +1100

    Fixed access_token, uses new random bytes decoding

--- backend/handlers/users/getUserData.go
+++ backend/handlers/users/getUserData.go
@@ -3,6 +3,7 @@ package users
 import (
 	"context"
 	"database/sql"
+	"encoding/base64"
 	"log"
 
 	"github.com/adamdgit/gotest/backend/api"
@@ -23,17 +24,25 @@ func GetUserData(db *sql.DB) fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		access_token := c.Cookies("access_token")
 
+		// incoming tokens must be decoded into binary (how they are stored in DB)
+		decoded_access, err := base64.RawURLEncoding.DecodeString(access_token)
+		if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(
+				api.ErrInternalServer,
+			)
+		}
+
 		// Get the user_id via session_id and check its valid
 		row := db.QueryRowContext(context.Background(),
 			`SELECT u.email, u.role, u.profile_url 
 			FROM sessions s 
 			JOIN users u ON s.user_id = u.id 
 			WHERE s.access_token = ?`,
-			access_token)
+			decoded_access)
 
 		var user models.User
 
-		err := row.Scan(&user.Email, &user.Role, &user.Profile_URL)
+		err = row.Scan(&user.Email, &user.Role, &user.Profile_URL)
 		if err != nil {
 			log.Printf("error getuserdata(): %s", err)
 			return c.Status(fiber.StatusInternalServerError).JSON(
@@ -41,9 +50,8 @@ func GetUserData(db *sql.DB) fiber.Handler {
 			)
 		}
 
-		log.Printf("--email: %s, role: %s", user.Email, user.Role)
 		// Success, return data as json
-		return c.Status(fiber.StatusOK).JSON(api.UserSessionRes{
+		return c.Status(fiber.StatusOK).JSON(api.UserDataRes{
 			Email:       user.Email,
 			Role:        user.Role,
 			Profile_URL: user.Profile_URL,
